03008edca7fd9202f32580bb2c98fbd4bfa6f793
Priyanka Patil <priyankapatil@Priyankas-MacBook-Pro.local>
2021-03-29 17:41:40 -0700

    added first program

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package prjAPITest;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i =0;i<10;i++)
+		{
+			System.out.println("hello Jenkins");
+		}
+
+	}
+
+}
